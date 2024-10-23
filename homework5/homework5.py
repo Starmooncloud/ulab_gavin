e05785c7e1cd44e0a06573e3eede5d0087805f83
Starmooncloud <gavin.tamasi@berkeley.edu>
2024-10-23 16:10:10 -0700

    added homework5

---
+++ homework5/homework5.py
@@ -0,0 +1,25 @@
+#Question 1
+# 1. pwd
+# 2. ls files should have patterns.txt and symmetry.py
+# 3. nano patterns.txt
+# 4. cat patterns.text
+# 6. mv patterns.text .. math/calculus
+# 7. cd .. math geomoetry 
+# rm triangle.txt
+# 8. cd .. math
+# 9. git init
+# 10. Using a SSH key
+# 11. cd ..music cp symmetry.txt ..math
+# 12. git It is currently tracking it, because you have interacted with it
+# 13. git add . symmetry.txt For all files: git add .
+# 14. It is, because git can see it
+# 15. git commmit -m "Copied symmetry.txt"
+# 16. git push main Should be pushing to main, because that is only branch currently
+# 17. git branch applied_math
+# 18. git checkout applied_math
+# 19. git push main to push them (after staging), if didn't push before switching then git doesn't remember changes
+# 20. git branch -a
+# 21. git checkout main
+# 23.
+#
+#
